730583737eb4f5db8d930ef61401c07c847a605e
Boris Starkov <boris.starkow@gmail.com>
2019-12-25 13:12:16 +0300

    add web page generation

--- src/Master.kt
+++ src/Master.kt
@@ -1,25 +1,97 @@
 package com.kotletaTesting
 
 import io.ktor.application.call
-import io.ktor.http.Parameters
-import io.ktor.request.*
+import io.ktor.html.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.Netty
 
+import io.ktor.request.receiveParameters
+import kotlinx.html.*
+
 fun main(args: Array<String>): Unit {
     val server = embeddedServer(Netty, 8000) {
         routing {
             get("/") {
-                call.respondText("Main Page")
+                call.respondHtml {
+                    head {
+                        title {
+                            + "kotleta"
+                        }
+                    }
+                    body {
+                        h2 {
+                            + "Problems:"
+                        }
+                        Storage.problems.forEach {
+                            ul {
+                                li {
+                                    a (href = "/problem/${it.value.id}"){
+                                        + it.value.title
+                                    }
+                                }
+                            }
+                        }
+                        a (href = "/new") {
+                            + "add new!"
+                        }
+                    }
+                }
             }
             get("/new") {
-                 call.respondText { "Create new problem" }
+                call.respondHtml {
+                    head {
+                        title {
+                            + "new problem"
+                        }
+                    }
+                    body {
+                        form {
+                            method = FormMethod.post
+                            action = "/new"
+                            + "Problem name:"
+                            br
+                            input (type = InputType.text) {
+                                name = "title"
+                            }
+                            br
+                            + "Problem description:"
+                            br
+                            textArea {
+                                name = "description"
+                            }
+                            br
+                            input (type = InputType.submit) {
+                                value = "submit"
+                            }
+                        }
+                    }
+                }
+            }
+            post("/new") {
+                val params = call.receiveParameters()
+                Storage.addProblem(params["title"] ?: "", params["description"] ?: "")
+                call.respondText { "the problem ${params["title"]} was created!" }
             }
             get("/problem/{id}") {
-                val id = call.parameters["id"]
-                call.respondText("Problem id = $id")
+                val id : Int = call.parameters["id"]?.toInt() ?: -1
+                val problem = Storage.problems[id]
+                call.respondHtml {
+                    head {
+                        title {
+                            + ("$problem")
+                        }
+                    }
+                    body {
+                        h1 {
+                            +("${problem?.title}")
+                        }
+                        div {
+                            +("${problem?.description}")
+                        }
+                    }
+                }
             }
         }
     }
